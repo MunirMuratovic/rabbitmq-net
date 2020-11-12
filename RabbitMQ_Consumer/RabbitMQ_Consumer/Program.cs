d9bb1a19c22852dc0b54d532eae338153f68b159
munjau <munirmuratovicx@gmail.com>
2020-11-12 13:51:16 +0100

    initial

---
+++ RabbitMQ_Consumer/RabbitMQ_Consumer/Program.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMQ_Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var queueName = "demo-queue";
+
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqp://munja:munja@localhost:5672/")
+            };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare(queueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (sender, e) => {
+                var body = e.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(message);
+            };
+
+            channel.BasicConsume(queueName, true, consumer);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RabbitMQ_Producer/RabbitMQ_Producer/Program.cs
@@ -0,0 +1,29 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Text.Json;
+
+namespace RabbitMQ_Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var queueName = "demo-queue";
+
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqp://guest:guest@localhost:5672")
+            };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare(queueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+            var message = new { Name = "Product", Message = "Hello!" };
+
+            var body = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(message));
+
+            channel.BasicPublish("", queueName, null, body);
+        }
+    }
+}
